22f5b0f8398fd345e35490a9aaf3611752c2b71d
Andrea Rosentel <andrearosentel@gmail.com>
2022-09-01 06:57:37 -0700

    commit tree eb6d16f084a7d1f559ce8d313b1609fcfb12744e

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,9 +1,7 @@
-class ShortPostSerializer < ActiveModel::Serializer
+class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content, :tags
-
+  has_many :tags
   def short_content
     "#{self.object.content[0..39]}..."
   end
-
-  
 end
